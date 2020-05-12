0d0785258c968f2dd93f540e4873ffac8c6457f2
Polina <p.kuptsova@yandex.ru>
2020-05-13 00:46:15 +0500

    commit computation

--- Kursach/src/plata/plata.java
+++ Kursach/src/plata/plata.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 
-//Главный класс, реализующий интерфейс ActionListener
+
 //Интерфейс нужен для обработки события нажатия на кнопку
 public class plata implements ActionListener{
     JFrame frame = new JFrame("Кварплата"); //главное окно
@@ -131,23 +131,118 @@ public class plata implements ActionListener{
         //возвращаем ссылку на кнопку
         return btn;
     }
+  //метод расчета
+  	public void kvartira() throws Exception {
+  		double c = 0;
+  		double h = 0;
+  		double s = 0;
+  		double g = 0;
+  		double k = 0;
+  		double vg = 0;
+  		double vh = 0;
+  		double vc = 0;
+  		double vs = 0;
+  		double u = 0;
+  		double d = 0;
+  		double p = 0;
+  		double q1 = 0;
+  		double q2 = 0;
+  		double q3 = 0;
+  		double q4 = 0;
+  		double q5 = 0;
+  		double summa = 0;
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new plata();
-            }
-        });
-    }
+  		for (int i = 0; i < 12; i++) {
+  			if (arrTf[i].getText().length() == 0) {
+  				arrTf[i].setText(" ");
+  			}
+  		}
 
+  		if (!arrTf[0].getText().equals(" ")) {
+  			c = Double.valueOf(arrTf[0].getText()); // cold water
+  		}
+  		if (!arrTf[1].getText().equals(" ")) {
+  			h = Double.valueOf(arrTf[1].getText()); // hot water
+  		}
+  		if (!arrTf[2].getText().equals(" ")) {
+  			s = Double.valueOf(arrTf[2].getText()); // svet
+  		}
+  		if (!arrTf[3].getText().equals(" ")) {
+  			g = Double.valueOf(arrTf[3].getText()); // gas
+  		}
+  		if (!arrTf[4].getText().equals(" ")) {
+  			k = Double.valueOf(arrTf[4].getText()); // kolichestvo projivayuchih
+  		}
+  		if (!arrTf[5].getText().equals(" ")) {
+  			vg = Double.valueOf(arrTf[5].getText()); // v potrachennogo gasa
+  		}
+  		if (!arrTf[6].getText().equals(" ")) {
+  			vh = Double.valueOf(arrTf[6].getText()); // v potrachennogo hot water
+  		}
+  		if (!arrTf[7].getText().equals(" ")) {
+  			vc = Double.valueOf(arrTf[7].getText()); // v potrachennogo cold water
+  		}
+  		if (!arrTf[8].getText().equals(" ")) {
+  			vs = Double.valueOf(arrTf[8].getText()); // v potrachennogo svet
+  		}
+  		if (!arrTf[9].getText().equals(" ")) {
+  			u = Double.valueOf(arrTf[9].getText()); // kommunalnye uslugy
+  		}
+  		if (!arrTf[10].getText().equals(" ")) {
+  			d = Double.valueOf(arrTf[10].getText()); // dolg
+  		}
+  		if (!arrTf[11].getText().equals(" ")) {
+  			p = Double.valueOf(arrTf[11].getText());// pereplata
+  		}
+  		
+  		
+  		if ((c != 0) & (vc != 0)) {
+  			q1 = c * vc; // Cena na cold water
+  		}
+  		if ((h != 0) & (vh != 0)) {
+  			q2 = h * vh; // Cena na hot water
+  		}
+  		if ((g != 0) & (vg != 0)) {
+  			q3 = g * vg; // Cena na gas
+  		}
+  		if ((s != 0) & (vs != 0)) {
+  			q4 = s * vs; // Cena na svet
+  		}
+  		if ((k != 0) & (u != 0)) {
+  			q5 = k * u; // Cena na kommunalnye uslugy
+  		}
+  		if ((q1 != 0) || (q2 != 0) || (q3 != 0) || (q4 != 0) || (q5 != 0) || (d >= 0) || (p >= 0)) {
+  			summa = q1 + q2 + q3 + q4 + q5 + d - p; // summa
+  	}
+  			
+  	}
+  	public static void main(String[] args) {
+  		SwingUtilities.invokeLater(new Runnable() {
+  		@Override
+  		public void run() {
+  		new plata();
+  		}
+  		});
+  		}
+  		//Метод обработки события нажатия на кнопку
+  		@Override
+  		public void actionPerformed(ActionEvent e) {
+  		//узнаем имя кнопки, на которую нажали
+  		if (e.getActionCommand().equals("Расчет")) {
+  		try {
+  		//выполняем расчет
+  		kvartira();
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-   
-    }
+  		} catch (Exception ex) {
 
+  		JOptionPane.showMessageDialog(null, "Проверьте правильность ввода");
+  		}
 
+  		}else{
+  		//очищаем все поля
+  		for(int i=0; i<arrTf.length; i++){
+  		arrTf[i].setText("");
+  		}
+  		}
+  		}
+  		}
